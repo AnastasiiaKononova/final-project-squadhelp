869665c2bc85d0a5c256f13c9c3bd35df1f50076
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-07-28 15:38:50 +0300

    fix: rename socket import in server file

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,12 @@
 const http = require('http');
 const app = require('./app');
 require('./dbMongo/mongoose');
-const controller = require('./socketInit');
+const ws = require('./socketInit');
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-server.listen(PORT,
-  () => console.log(`Example app listening on port ${ PORT }!`));
-controller.createConnection(server);
+server.listen(PORT, () => console.log(`Example app listening on port ${ PORT }!`));
+ws.createConnection(server);
 
 
