30447797aaa6811d021b548d1d1fff39a730f891
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-08-04 12:53:54 +0300

    refactor(controllers): extract and update payment logic from User.controller to Banking.controller

---
+++ server/src/controllers/Banking.controller.js
@@ -0,0 +1,89 @@
+const bd = require('../models');
+const { transferFunds } = require('./queries/bankQueries');
+const { calculatePrize, generateOrder } = require('../utils/orderHelper');
+const NotEnoughMoney = require('../errors/NotEnoughMoney');
+const ServerError = require('../errors/ServerError');
+const { findUserWithLock } = require('./queries/userQueries');
+
+module.exports.payment = async (req, res, next) => {
+  const userCardNumber = req.body.number.replace(/ /g, '');
+  const price = Number(req.body.price);
+
+  let transaction;
+  try {
+    transaction = await bd.sequelize.transaction();
+
+    await transferFunds({
+      userCardNumber,
+      cvc: req.body.cvc,
+      expiry: req.body.expiry,
+      amount: price,
+      transaction,
+    });
+
+    const { orderId, createdAt } = generateOrder();
+
+    const contestsData = req.body.contests.map((contest, index) => {
+      const file = req.files?.[index];
+      return {
+        ...contest,
+        status: index === 0 ? 'active' : 'pending',
+        userId: req.tokenData.userId,
+        priority: index + 1,
+        orderId,
+        createdAt,
+        prize: calculatePrize(price, req.body.contests.length, index),
+        fileName: file ? file.filename : null,
+        originalFileName: file ? file.originalname : null,
+      };
+    });
+
+    await bd.Contests.bulkCreate(contestsData, { transaction });
+
+    await transaction.commit();
+
+    res.status(200).send({ success: true, orderId });
+  } catch (err) {
+    if (transaction) await transaction.rollback();
+    next(err);
+  }
+};
+
+module.exports.cashout = async (req, res, next) => {
+  const userCardNumber = req.body.number.replace(/ /g, '');
+  const payoutAmount = Number(req.body.sum);
+
+  let transaction;
+  try {
+    transaction = await bd.sequelize.transaction();
+
+    const user = await findUserWithLock(req.tokenData.userId, transaction);
+
+    if (!user) {
+      throw new ServerError('User not found');
+    }
+
+    if (user.balance < payoutAmount) {
+      throw new NotEnoughMoney('Not enough funds on your account');
+    }
+
+    await transferFunds({
+      userCardNumber,
+      cvc: req.body.cvc,
+      expiry: req.body.expiry,
+      amount: -payoutAmount,
+      transaction,
+    });
+
+    user.balance -= payoutAmount;
+    await user.save({ transaction });
+
+    await transaction.commit();
+
+    res.status(200).send({ success: true, balance: user.balance });
+  } catch (err) {
+    if (transaction) await transaction.rollback();
+    next(err);
+  }
+};
+
