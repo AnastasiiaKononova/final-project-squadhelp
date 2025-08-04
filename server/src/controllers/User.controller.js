4a806ffeb52acab3b82b5d20b43c04a184bb3d17
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-08-04 14:03:10 +0300

    refactor(user): extract rating logic to ratingQueries module

--- server/src/controllers/User.controller.js
+++ server/src/controllers/User.controller.js
@@ -1,45 +1,19 @@
 const bd = require('../models');
 const ws = require('../socketInit');
-const userQueries = require('./queries/authQueries');
+const userQueries = require('./queries/userQueries');
 const ratingQueries = require('./queries/ratingQueries');
-
-function getQuery (offerId, userId, mark, isFirst, transaction) {
-  const getCreateQuery = () => ratingQueries.createRating({
-    offerId,
-    mark,
-    userId,
-  }, transaction);
-  const getUpdateQuery = () => ratingQueries.updateRating({ mark },
-    { offerId, userId }, transaction);
-  return isFirst ? getCreateQuery : getUpdateQuery;
-}
 
 module.exports.changeMark = async (req, res, next) => {
   let transaction;
   const { isFirst, offerId, mark, creatorId } = req.body;
-  const userId = req.tokenData.userId;
+  const { userId } = req.tokenData;
 
   try {
     transaction = await bd.sequelize.transaction(
-      { isolationLevel: bd.Sequelize.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED });
+      { isolationLevel: bd.Sequelize.Transaction.ISOLATION_LEVELS.READ_COMMITTED });
 
-    const query = getQuery(offerId, userId, mark, isFirst, transaction);
-    await query();
-
-    const offersArray = await bd.Ratings.findAll({
-      include: [
-        {
-          model: bd.Offers,
-          required: true,
-          where: { userId: creatorId },
-        },
-      ],
-      transaction,
-    });
-
-    const sum = offersArray.reduce((acc, curr) => acc + curr.dataValues.mark, 0);
-    const avg = offersArray.length ? (sum/offersArray.length) : 0 ;
-
+    await ratingQueries.createOrUpdateRating({ offerId, userId, mark, isFirst }, transaction);
+    const avg = await ratingQueries.calcRatingAvgByUser(creatorId, transaction);
     await userQueries.updateUser({ rating: avg }, creatorId, transaction);
     await transaction.commit();
 
--- server/src/controllers/queries/ratingQueries.js
+++ server/src/controllers/queries/ratingQueries.js
@@ -1,7 +1,7 @@
 const bd = require('../../models');
 const ServerError = require('../../errors/ServerError');
 
-module.exports.updateRating = async (data, predicate, transaction) => {
+const _updateRating = async (data, predicate, transaction) => {
   const [updatedCount, [updatedRating]] = await bd.Ratings.update(data,
     { where: predicate, returning: true, transaction });
   if (updatedCount !== 1) {
@@ -10,13 +10,32 @@ module.exports.updateRating = async (data, predicate, transaction) => {
   return updatedRating.get({ plain: true });
 };
 
-module.exports.createRating = async (data, transaction) => {
+const _createRating = async (data, transaction) => {
   try {
-    const result = await bd.Ratings.create(data, {transaction});
+    const result = await bd.Ratings.create(data, { transaction });
     return result.get({ plain: true });
   } catch {
-    throw new ServerError('Cannot mark offer')
+    throw new ServerError('Cannot mark offer');
   }
 };
- 
+
+module.exports.createOrUpdateRating = async (data, transaction) => {
+  const { offerId, userId, isFirst } = data;
+  return isFirst ? _createRating(data, transaction) : _updateRating(data, { offerId, userId }, transaction);
+};
+
+module.exports.calcRatingAvgByUser = async (userId, transaction) => {
+  const offersArray = await bd.Ratings.findAll({
+    include: [
+      {
+        model: bd.Offers,
+        required: true,
+        where: { userId },
+      },
+    ],
+    transaction,
+  });
+  const sum = offersArray.reduce((acc, curr) => acc + curr.dataValues.mark, 0);
+  return offersArray.length ? (sum/offersArray.length) : 0 ;
+};
 
