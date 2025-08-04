b9dac3c7654898f4a99da132c5a3f261f986072c
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-08-04 12:39:00 +0300

    refactor(utils): rename jwtUtils.js to authUtils.js and update code

--- server/src/utils/authUtils.js
+++ server/src/utils/authUtils.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const CONSTANTS = require('../constants');
+const UncorrectPassword = require('../errors/UncorrectPassword');
 
 module.exports.generateAccessToken = (user) => jwt.sign({
   firstName: user.firstName,
@@ -12,3 +14,11 @@ module.exports.generateAccessToken = (user) => jwt.sign({
   email: user.email,
   rating: user.rating,
 }, CONSTANTS.JWT_SECRET, { expiresIn: CONSTANTS.ACCESS_TOKEN_TIME });
+
+module.exports.passwordCompare = async (pass1, pass2) => {
+  const passwordCompare = await bcrypt.compare(pass1, pass2);
+  if (!passwordCompare) {
+    throw new UncorrectPassword('Wrong password');
+  }
+};
+
