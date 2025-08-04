78833052fca070e2c52f1ff724ecbd2501a31dc7
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-08-04 12:43:30 +0300

    refactor(Auth.controller): extract DB logic to queries

--- server/src/controllers/Auth.controller.js
+++ server/src/controllers/Auth.controller.js
@@ -1,18 +1,15 @@
-const jwt = require('jsonwebtoken');
-const CONSTANTS = require('../constants');
-const authQueries = require('./queries/authQueries');
-const { generateAccessToken } = require('../utils/jwtUtils');
+const userQueries = require('./queries/userQueries');
+const { generateAccessToken, passwordCompare } = require('../utils/authUtils');
 const NotUniqueEmail = require('../errors/NotUniqueEmail');
-const TokenError = require('../errors/TokenError');
 const UserNotFoundError = require('../errors/UserNotFoundError');
 
 module.exports.login = async (req, res, next) => {
   try {
-    const foundUser = await authQueries.findUser({ email: req.body.email });
-    await authQueries.passwordCompare(req.body.password, foundUser.password);
+    const foundUser = await userQueries.findUser({ email: req.body.email });
+    await passwordCompare(req.body.password, foundUser.password);
 
     const accessToken = generateAccessToken(foundUser);
-    await authQueries.updateUser({ accessToken }, foundUser.id);
+    await userQueries.updateUser({ accessToken }, foundUser.id);
 
     res.send({ token: accessToken });
   } catch (err) {
@@ -22,12 +19,14 @@ module.exports.login = async (req, res, next) => {
 
 module.exports.registration = async (req, res, next) => {
   try {
-    const newUser = await authQueries.userCreation(
-      Object.assign(req.body, { password: req.hashPass }),
-    );
+    const { hashPass, body } = req;
+    const newUser = await userQueries.userCreation({
+      ...body,
+      password: hashPass,
+    });
 
     const accessToken = generateAccessToken(newUser);
-    await authQueries.updateUser({ accessToken }, newUser.id);
+    await userQueries.updateUser({ accessToken }, newUser.id);
 
     res.send({ token: accessToken });
   } catch (err) {
@@ -40,16 +39,9 @@ module.exports.registration = async (req, res, next) => {
 };
 
 module.exports.me = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return next(new TokenError('Authorization token is required'));
-  }
-
-  const accessToken = authHeader.replace(/^Bearer\s+/i, '');
-
   try {
-    const tokenData = jwt.verify(accessToken, CONSTANTS.JWT_SECRET);
-    const foundUser = await authQueries.findUser({ id: tokenData.userId });
+    const { tokenData } = req;
+    const foundUser = await userQueries.findUser({ id: tokenData.userId });
 
     res.send({
       firstName: foundUser.firstName,
@@ -63,13 +55,8 @@ module.exports.me = async (req, res, next) => {
     });
   } catch (err) {
     if (err instanceof UserNotFoundError) {
-      return next(new TokenError('User not found'));
+      return next(new UserNotFoundError('User not found'));
     }
-
-    if (err.name === 'JsonWebTokenError' || err.name === 'TokenExpiredError') {
-      return next(new TokenError('Invalid or expired token'));
-    }
-
     next(err);
   }
 };
