984a22b1641d06655bd00fe9f144c0341723cf7f
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-09-25 14:47:27 +0300

    refactor(auth): fix reducer and sagas

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -9,18 +9,21 @@ export default function (state = initialState, action) {
   switch (action.type) {
     case ACTION.AUTH_ACTION_REQUEST: {
       return {
+        ...state,
         isFetching: true,
         error: null,
       };
     }
     case ACTION.AUTH_ACTION_SUCCESS: {
       return {
+        ...state,
         isFetching: false,
         error: null,
       };
     }
     case ACTION.AUTH_ACTION_ERROR: {
       return {
+        ...state,
         isFetching: false,
         error: action.error || null,
       };
